15479647c7ddc35e262e817a1a6d09095083431c
VG <15960525+brillcp@users.noreply.github.com>
2026-03-09 17:47:22 +0100

    Initial Commit

---
+++ Bildhantering/BildhanteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BildhanteringApp.swift
+//  Bildhantering
+//
+//  Created by Viktor Gidlöf on 2026-03-09.
+//
+
+import SwiftUI
+
+@main
+struct BildhanteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bildhantering/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bildhantering
+//
+//  Created by Viktor Gidlöf on 2026-03-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
